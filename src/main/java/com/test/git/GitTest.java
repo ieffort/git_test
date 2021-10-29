6aaf0eb6ff2eba841a248c879d0d24e5c80e7d06
tom_glb <goodMorning_pro@163.com>
2021-10-29 15:05:45 +0800

    hot_fix commit1

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello world");
         System.out.println("hello young");
+        System.out.println( "hello tomorrow");
     }
 }
